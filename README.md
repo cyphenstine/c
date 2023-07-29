ee61dfbd24b60062613f3cda8f70007c46111b93
cyphenstine <aritrakumar093@gmail.com>
2023-07-29 21:29:49 +0530

    creating the calculater

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 "# initial_c_programing_practice" 
 
 here are my initial c programing codes , i am practiceing c !!
+
+and i also tried make a calculater
--- calculater.c
+++ calculater.c
@@ -5,11 +5,17 @@ void main()
 {
 
     // a calculater that could take unlimited intups , but two at a time and can calculate " sum , difference , multiplication , divition , squre , root , cube , root over 3 ""
-    // you can choose what you wanna do from above options by writting that 
+    // you can choose what you wanna do from above options by writting that oparation
 
-    int x,y,v;
+    double x=4.1,y=4.9,sum=0;
+    char oparation;
 
-    
+    printf("what do you want : ");
+    scanf("%c",& oparation);
+    if (oparation==sum)
+    {
+        printf("%lf",x+y);
+    }
     
 
 
