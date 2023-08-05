55e13ce774d0c3555e7fb43a2e7d840df1fae86c
cyphenstine <aritrakumar093@gmail.com>
2023-08-05 20:52:54 +0530

    added "/" and "-" operation

--- calculater.c
+++ calculater.c
@@ -4,8 +4,8 @@
 void main()
 {
 
-    // a calculater that could take unlimited intups , but two at a time and can calculate " sum , difference , multiplication , divition , squre , root , cube , root over 3 "
-    // you can choose what you wanna do from above options by writting that oparation
+    // a calculater that could take unlimited intups , but two at a time then after that oparation it will take only one number infinite times and can calculate " sum , difference , multiplication , divition , squre , root , cube , root over 3 "
+    // you can choose what you wanna do from above options by entering that operation sign that oparation and sometimes writting 
     char opa;
     double x,y,result,nxt;
     printf("what operation do you want : ");
@@ -22,7 +22,7 @@ void main()
         {
             printf("\nenter next number : ");
             scanf("%lf",&nxt);
-            result=nxt+result;
+            result=result+nxt;
             printf("result : %lf",result);
         }
     }
@@ -38,13 +38,41 @@ void main()
         {
             printf("\nenter next number : ");
             scanf("%lf",&nxt);
-            result=nxt*result;
+            result=result*nxt;
             printf("result : %lf",result);
         }
     }
-    else
+    else if(opa=='/')
     {
-        printf("\nshut the fuck up , operation not available yet !!");
+        printf("enter number : ");
+        scanf("%lf",&x);
+        printf("enter number : ");
+        scanf("%lf",&y);
+        result=x/y;
+        printf("result : %lf",result);
+         while(1)
+        {
+            printf("\nenter next number : ");
+            scanf("%lf",&nxt);
+            result=result/nxt;
+            printf("result : %lf",result);
+        }
+    }
+    else if(opa=='-')
+    {        
+        printf("enter number : ");
+        scanf("%lf",&x);
+        printf("enter number : ");
+        scanf("%lf",&y);
+        result=x-y;
+        printf("result : %lf",result);
+         while(1)
+        {
+            printf("\nenter next number : ");
+            scanf("%lf",&nxt);
+            result=result-nxt;
+            printf("result : %lf",result);
+        }
     }
     
 
