ff339a62d6d3e7156e4cb51ce7f6633004319b05
cyphenstine <aritrakumar093@gmail.com>
2023-08-06 13:55:39 +0530

    added squreroot,sqrt,qube

--- calculater.c
+++ calculater.c
@@ -7,7 +7,7 @@ void main()
     // a calculater that could take unlimited intups , but two at a time then after that oparation it will take only one number infinite times and can calculate " sum , difference , multiplication , divition , squre , root , cube , root over 3 "
     // you can choose what you wanna do from above options by entering that operation sign that oparation and sometimes writting 
     char opa;
-    double x,y,result,nxt;
+    double x,y,result,nxt,s,sqroot,q;
     printf("what operation do you want : ");
     scanf("%c",&opa);
     if(opa=='+')
@@ -74,10 +74,27 @@ void main()
             printf("result : %lf",result);
         }
     }
-    
-
-
-
+    else if(opa=='^')
+    {
+        printf("enter the number : ");
+        scanf("%lf",&s);
+        s=s*s;
+        printf("squre : %lf",s);
+    }
+    else if(opa=='r')
+    {
+        printf("enter number : ");
+        scanf("%lf",&sqroot);
+        sqroot=sqrt(sqroot);
+        printf("squre root : %lf",sqroot);
+    }
+    else if(opa=='q')
+    {
+        printf("enter the number : ");
+        scanf("%lf",&q);
+        q=q*q*q;
+        printf("qube : %lf",q);
+    }
 
 
 
