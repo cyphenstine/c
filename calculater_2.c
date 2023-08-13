ca41b477961fb77da420d1990d600e8b2a74029f
cyphenstine <aritrakumar093@gmail.com>
2023-08-14 00:18:06 +0530

    update : to the power

--- calculater_2.c
+++ calculater_2.c
@@ -2,7 +2,8 @@
 #include<math.h>
 void main()
 {
-  double n1,n2,nxt,result;
+  double n1,n2,result;
+  int pow,i=1;
   char opa;
   printf("enter number : ");
   scanf("%lf",&n1);  
@@ -10,27 +11,74 @@ void main()
   {
     printf("oparetion : ");
     scanf(" %c", &opa);
+    // sum :
     if(opa=='+')
     {
-      
       printf("enter number : ");
       scanf("%lf" , & n2);
       result=n1+n2;
       printf("result : %lf\n" , result);
-      n1=result;
-
-      
-
-      
-    
-      
-      
-
-      
-      
-    
- 
+      n1=result; 
     }
+    // diff :
+    else if(opa=='-')
+    {
+      printf("enter number : ");
+      scanf("%lf" , & n2);
+      result=n1-n2;
+      printf("result : %lf\n" , result);
+      n1=result; 
+    }
+    // mul :
+    else if(opa=='*')
+    {
+      printf("enter number : ");
+      scanf("%lf" , & n2);
+      result=n1*n2;
+      printf("result : %lf\n" , result);
+      n1=result; 
+    }
+    // div :
+    else if(opa=='/')
+    {
+      printf("enter number : ");
+      scanf("%lf" , & n2);
+      result=n1/n2;
+      printf("result : %lf\n" , result);
+      n1=result; 
+    }
+    // power :
+    else if (opa == '^') 
+    {
+      printf("enter power : ");
+      scanf("%d", &pow);
+    
+      result = 1; // Initialize result to 1 for correct power calculation
+      for (i = 0; i < pow; i++) 
+      {
+        result = result * n1; // Multiply n1 by itself 'pow' times
+      }
+      printf("result : %lf\n", result);
+    
+    }
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   }                                                                                
   
 
