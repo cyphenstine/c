b72390fccc212dcf44210e3fb48a61cc0501ca69
cyphenstine <aritrakumar093@gmail.com>
2023-08-14 18:15:08 +0530

    nothing special

--- calculater_2.c
+++ calculater_2.c
@@ -6,7 +6,8 @@ void main()
   int pow,i=1;
   char opa;
   printf("enter number : ");
-  scanf("%lf",&n1);  
+  scanf("%lf",&n1); 
+   
   while(1)  
   {
     printf("oparetion : ");
@@ -46,38 +47,7 @@ void main()
       result=n1/n2;
       printf("result : %lf\n" , result);
       n1=result; 
-    }
-    // power :
-    else if (opa == '^') 
-    {
-      printf("enter power : ");
-      scanf("%d", &pow);
-    
-      result = 1; // Initialize result to 1 for correct power calculation
-      for (i = 0; i < pow; i++) 
-      {
-        result = result * n1; // Multiply n1 by itself 'pow' times
-      }
-      printf("result : %lf\n", result);
-    
-    }
-    
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    } 
     
   }                                                                                
   
