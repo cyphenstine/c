040fe02e374cf692f511404d6b7a4ba69b4cac17
cyphenstine <aritrakumar093@gmail.com>
2023-09-20 23:14:09 +0530

    pattern print

--- practice.c
+++ practice.c
@@ -716,6 +716,34 @@ int main()
     scanf("%d", &n);
     */
 
+
+    // ✌️#29 pattern printing
+    /*
+    pattern : 
+    *
+    **
+    ***
+    ****
+    *****
+    */
+
+
+    /*
+    int n , i , r ,j;
+    char c='*';
+    printf("rows : ");
+    scanf("%d", &n);
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c", c);
+        }
+        printf("\n"); 
+    }
+    */
+
     
 
     
