ddbdca2846bfaee786251bcde3437010c9e0bd87
cyphenstine <aritrakumat093@gmail.com>
2023-11-14 01:22:09 +0530

    used facto function

--- functions.c
+++ functions.c
@@ -67,37 +67,51 @@ void greet()
 
 int facto(int n)
 {
-    int fact, i;
+    int fact=1, i;
     for(i=1;i<=n;i++)
     {
         fact=fact*i;
     }
-    printf("%d",fact);
+    //printf("%d",fact);
     return fact;
 }
 int main()
 {
-    int n,i,r,fact_n=1,fact_r=1,j,sub,fact_sub=1,k,combi;
+    int n,i,r,fact_n,fact_r,j,sub,fact_sub,k,combi;
     printf("enter n : ");
     scanf("%d", &n); 
     printf("enter r : ");
     scanf("%d", &r);   
     //n!
-    for(i=1;i<=n;i++)
+    // beta way
+    /*    for(i=1;i<=n;i++)
     {
         fact_n=fact_n*i;
-    }    
+    }   
+    */
+    // chad way
+    fact_n=facto(n);
     //r!
+    // beta way
+    /*
     for(j=1;j<=r;j++)
     {
         fact_r=fact_r*j;
     }
+    */
+    // chad way
+    fact_r=facto(r);
     //sub!
     sub=n-r;
+    // beta way
+    /*
     for(k=1;k<=sub;k++)
     {
         fact_sub=fact_sub*k;
     }
+    */
+    // chad way
+    fact_sub=facto(sub);
     //combination
     combi=fact_n/(fact_r*fact_sub);
     printf("%d", combi);
