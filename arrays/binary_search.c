e1449b7e7c9dd168577d7da3b23e2d221169d41c
cyphenstine <aritrakumat093@gmail.com>
2023-11-14 15:29:16 +0530

    ns

--- arrays/binary_search.c
+++ arrays/binary_search.c
@@ -1,49 +1,6 @@
 #include<stdio.h>
-#include<stdbool.h>
 int main()
 {
-    
-
-
-        
-
-    
-
-
-
-
-
-    
-
-    //✌️#12 linear search with bool data type
-    /*
-    int n , arr[7]={2,5,7,8,6,4,9}, i;
-    bool marker = 0;
-    printf("enter number : ");
-    scanf("%d", &n);
-    for(i=0 ; i<7 ; i++)
-    {
-        if (n==arr[i])
-        {
-            marker = 1;
-            break;
-        }
-    }
-    if(marker==1)
-    {
-        printf("found");
-    }
-    else
-    {
-        printf("not found");
-    }
-    */
-
-
-    //✌️#13 binary search & bubble sort 
-
-
-    /*
     int arr[100], lb , ub , n , lim , mid , i , f=0;
     printf("how many you wanna work with : ");
     scanf("%d", &lim);
@@ -90,39 +47,7 @@ int main()
         {
             printf("not found");
         }
-    
     }
-    */
-
-
-    //bubble sort
-
-        //input
-        int n,i;
-        int arr[100];
-        printf("how many numbers : ");
-        scanf("%d", &n);
-        for(i=0 ; i<n ; i++)
-        {
-            printf("enter array : ");
-            scanf("%d", &arr[i]);
-        }
-
-        // sorting 
-
-        
-        
-        
-        
-    
-
-
-    
-
-
-
-    
-    
-    
     return 0;
+    
 }
---
+++ arrays/bubble_sort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    //input
+        int n,i;
+        int arr[100];
+        printf("how many numbers : ");
+        scanf("%d", &n);
+        for(i=0 ; i<n ; i++)
+        {
+            printf("enter array : ");
+            scanf("%d", &arr[i]);
+        }
+
+        // sorting 
+
+    
+    return 0;
+}
