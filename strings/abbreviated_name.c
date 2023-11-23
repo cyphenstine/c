3fde2d1a3602a2eab53f386c72c1bba19ee6c87b
cyphenstine <aritrakumat093@gmail.com>
2023-11-24 05:13:16 +0530

    started strings

---
+++ strings/abbreviated_name.c
@@ -0,0 +1,12 @@
+// take a string , it will contain a full name , print the initial of first name , initial of middle name and print full last name 
+#include<stdio.h>
+int main()
+{
+    char name[30];
+    printf("enter your name : ");
+    scanf("%[^\n]s",name);
+    //puts(name);
+    
+
+    return 0;
+}
---
+++ strings/notes.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    //initialization :
+    char str[40];
+    //input :
+    printf("enter : "); 
+    scanf("%[^\n]s",str);
+    //output :
+    printf("your input was : "); 
+    puts(str);
+    
+    
+
+    
+
+
+
+    return 0;
+    
+}
