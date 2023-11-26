afe342328ca7667dc2b7f40f340a27f2db046996
cyphenstine <aritrakumat093@gmail.com>
2023-11-27 04:12:15 +0530

    added abbribiated_name.c

--- strings/abbreviated_name.c
+++ strings/abbreviated_name.c
@@ -1,11 +1,46 @@
-// take a string , it will contain a full name , print the initial of first name , initial of middle name and print full last name 
+// take a string , it will contain a full name , print the initial of first name , initial of middle name(s) and print full last name 
 #include<stdio.h>
 int main()
 {
+    // taking the name 
     char name[30];
     printf("enter your name : ");
-    scanf("%[^\n]s",name);
-    //puts(name);
+    scanf("%[^\n]s", name);
+    // size of the input string 'or' number of letters in your name 
+    int len;
+    for(len=0 ; name[len] != '\0' ; len++){}
+        printf("size : %d", len);
+    // how many spaces in the name and capturing the position of the last sapce 
+    int space = 0 , j , pos[30] , p;
+    for(j = 0 ; name[j] != '\0' ; j++)
+    {
+        if(name[j] == ' ')
+            pos[p] = j;
+            p ++;
+            space ++;
+    }
+    printf("\nspaces in you name : %d",space);
+    // positions of the last space
+    
+    // @ abbriviation part
+    int k;
+    printf("\nyour name : ");
+    // prints the first letter of the name
+    printf("%c.", name[0]);
+    for(k = 0 ; name[k] != '\0' ; k++)
+    {
+        // abbriviates middle name(s)
+        if(name[k] == ' ')
+            printf("%c.", name[k+1]);
+        // printing the full last name
+        //else if()
+     
+    }
+        
+
+
+
+
     
 
     return 0;
