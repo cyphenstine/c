2a0e155d9312b822fe605409a3138750cd8e1c17
cyphenstine <aritrakumat093@gmail.com>
2023-12-09 20:46:37 +0530

    ns

--- strings/abb_name.c
+++ strings/abb_name.c
@@ -1,5 +1,6 @@
 // take a string , it will contain a full name , print the initial of first name , initial of middle name(s) and print full last name 
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     // taking the name 
@@ -11,13 +12,17 @@ int main()
     for(len=0 ; name[len] != '\0' ; len++){}
         printf("size : %d", len);
     // how many spaces in the name and capturing the position of the last sapce 
-    int space = 0 , j , posi[30] , p;
-    for(j = 0 ; name[j] != '\0' ; j++)
+    int space = 0 , j , pos[30] , p=0;
+    for(j = 0 ; name[j] != len ; j++)
     {
         if(name[j] == ' ')
-            posi[p]=j;
+        {
+            pos[p]=j;
             p++;
             space ++;
+
+        }
+            
     }
     printf("\nspaces in you name : %d",space);
     // @ abbriviation part
@@ -25,14 +30,14 @@ int main()
     printf("\nyour name : ");
     // prints the first letter of the name
     printf("%c.", name[0]);
+    // abbriviates middle name(s)
     for(k = 0 ; name[k] != '\0' ; k++)
     {
-        // abbriviates middle name(s)
         if(name[k] == ' ')
             printf("%c.", name[k+1]);     
     }
     // printing the full last name
-    for(k = posi[p] ; k <= len ; k++)
+    for(k = pos[p] ; k <= len ; k++)
         printf("%c" , name[k]);
         
 
